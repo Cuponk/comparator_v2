61270c62936946e8ee9778f07b2164d1078f59a8
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2026-01-27 18:51:10 -0800

    edit mappers and ingest

---
+++ .history/app/lib/mlb-api_20260127185059.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client/extension";
+
+const baseUrl = process.env.MLB_URL;
+
+//only fetch judge and ohtani for now
+export async function fetchPlayer(playerId: number) {
+    const url = baseUrl + "people/" + playerId;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Response code:' + response.status);
+        }
+        const result = await response.json();
+        const formatted = result.people[0];
+        return formatted;
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(error.message);
+        } else {
+            console.error("Unknown Error")
+        }
+        throw error;
+    }
+}
